6a7d19d3a8e80b4c2fc2e39700f10622ecc5a58c
Uday Verma <uday.karan@gmail.com>
2015-02-24 08:37:52 -0600

    Rip some components out, update pipeline

--- Procfile
+++ Procfile
@@ -2,5 +2,3 @@ js: sh -c 'cd lib && npm run dev'
 ww: sh -c 'cd workers && gulp watch'
 cl: sh -c 'cd renderer && lein cljsbuild auto dev'
 web: env PORT=3000 http-server
-p2p: peerjs --port 9000 --key hobu
-etcd: sh -c 'cd etcd && ./etcd --cors=*'
--- renderer/scripts/post-compile.sh
+++ renderer/scripts/post-compile.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 cat ./vendor/THREE.js \
     ./vendor/THREE.BinaryLoader.js \
     ./vendor/gl-matrix.js \
     $1/renderer.js > plasio-renderer.js
 echo "Wrote plasio-renderer.js"
-echo "${@:2}" | terminal-notifier -title "cljsbuild"
