57b78086f89c27abb5cba955dfffcb78969fa741
Uday Verma <verma@psg.local>
2015-10-20 21:27:06 -0500

    Hack hack, chomp chomp, refactor

--- workers/decompress.js
+++ workers/decompress.js
@@ -48,53 +48,60 @@ function collectStats(buffer, pointSize, numPoints) {
 	return {z: bin};
 }
 
-var hasColor = function(schema) {
-	var hasr = false, hasg = false, hasb = false;
-	for (var i = 0, il = schema.length ; i < il ; i ++) {
-		hasr = hasr || schema[i].name === "Red";
-		hasg = hasg || schema[i].name === "Green";
-		hasb = hasb || schema[i].name === "Blue";
-	}
+var unpackBuffer = function(buffer, totalPoints, pointSize, schema) {
+	var view = new DataView(buffer);
+	var woff = 0;
+	var roff = 0;
 
-	return hasr && hasb && hasg;
-};
-
-var addColor = function(input, numPoints, pointSize) {
-	// if we're adding color the point size is going to be 3 floats more
+	// convert our schema into a bunch of function calls
 	//
-	var newPointSize = pointSize + 12;
-	var newBuffer = new Float32Array(numPoints * newPointSize / 4);
+	var fields = [];
+	var computedPointSize = 0;
+	for (var i = 0 ; i < schema.length ; i ++) {
+		var s = schema[i];
 
-	var off = 0,
-		woff = 0;
-	var leftOver = pointSize - 12,
-		instride = pointSize / 4,
-		outstride = newPointSize / 4;
+		if (s.type === "floating" && s.size === 4)
+			fields.push([4, DataView.prototype.getFloat32]);
+		else if (s.type === "unsigned" && s.size === 2)
+			fields.push([2, DataView.prototype.getUint16]);
+		else if (s.type === "unsigned" && s.size === 4)
+			fields.push([4, DataView.prototype.getUint32]);
+		else
+			throw Error("Unrecognized schema field: " + JSON.stringify(s));
 
-	for (var i = 0 ; i < numPoints ; i ++) {
-		newBuffer[woff + 0] = input[off + 0];
-		newBuffer[woff + 1] = input[off + 1];
-		newBuffer[woff + 2] = input[off + 2];
-
-		// color
-		newBuffer[woff + 3] = 0;
-		newBuffer[woff + 4] = 0;
-		newBuffer[woff + 5] = 0;
-
-		// whatever else
-		for (var j = 0, jl = leftOver / 4 ; j < jl ; j ++) {
-			newBuffer[woff + 6 + j] = input[off + 3 + j];
-		}
-
-		off += instride;
-		woff += outstride;
+		computedPointSize += s.size;
 	}
 
-	return newBuffer;
+	if (computedPointSize !== pointSize) {
+		throw new Error("Point size validation failed, the schema size doesn't match computed point size");
+	}
+
+	// from this point on, everything is stored as 32-bit floats
+	var outBuffer = new Float32Array(totalPoints * schema.length);
+
+	console.log("-- -- TPW:", totalPoints);
+
+	for (var i = 0 ; i < totalPoints ; i ++) {
+		for (var j = 0, jl = fields.length ; j < jl ; j ++) {
+			var f = fields[j];
+
+			var size = f[0];
+			var fn = f[1];
+
+			outBuffer[woff] = fn.call(view, roff, true);
+
+			woff ++;
+			roff += size;
+		}
+	}
+
+	console.log("-- -- end of line:", woff * 4, roff);
+
+	return outBuffer;
 };
 
 var totalSaved = 0;
-var decompressBuffer = function(schema, worldBoundsX, ab, numPoints, checkAddColor) {
+var decompressBuffer = function(schema, worldBoundsX, ab, numPoints) {
 	var x = new Module.DynamicLASZip();
 
 	var abInt = new Uint8Array(ab);
@@ -104,13 +111,16 @@ var decompressBuffer = function(schema, worldBoundsX, ab, numPoints, checkAddCol
 	x.open(buf, ab.byteLength);
 
 	var pointSize = 0;
+	var needUnpack = false;
 
 	schema.forEach(function(f) {
 		pointSize += f.size;
 		if (f.type === "floating")
 			x.addFieldFloating(f.size);
-		else if (f.type === "unsigned")
+		else if (f.type === "unsigned") {
 			x.addFieldUnsigned(f.size);
+			needUnpack = true;
+		}
 		else
 			throw new Error("Unrecognized field desc:", f);
 	});
@@ -128,11 +138,17 @@ var decompressBuffer = function(schema, worldBoundsX, ab, numPoints, checkAddCol
 	Module._free(out);
 	Module._free(buf);
 
-	var b = new Float32Array(ret.buffer);
+	// we only need to unpack buffer if we have any non-floating point items in schema
+	//
+	var b = needUnpack ?
+		unpackBuffer(ret.buffer, numPoints, pointSize, schema) :
+		new Float32Array(ret.buffer);
 
-	if (checkAddColor && numPoints > 0 && !hasColor(schema)) {
-		b = addColor(b, numPoints, pointSize);
-	}
+	// the point size beyond this point has probably been updated, if the unpack happened we
+	// our point size is now different than what it was before, its always going to be
+	// 4 bytes per components since everything is converted to floats.
+	//
+	pointSize = schema.length * 4;
 
 	// if we got any points, swap them
 	if (numPoints > 0)
@@ -150,13 +166,11 @@ self.onmessage = function(e) {
 	var ab = data.buffer;
 	var numPoints = data.pointsCount;
 	var worldBoundsX = data.worldBoundsX;
-	var addColor = data.addColor;
 
-	var w = decompressBuffer(schema, worldBoundsX, ab, numPoints, addColor);
+	var w = decompressBuffer(schema, worldBoundsX, ab, numPoints);
 
 	var res = w[0],
 		stats = w[1];
-
 
 	postMessage({result: res, stats: stats}, [res.buffer]);
 };
